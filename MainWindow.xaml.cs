d048fdaf16173e4ad34d33acd7b438e0cb3ce71c
zjchenxk <zjchenxk@163.com>
2026-06-22 17:19:55 +0800

    在MainWindow.xaml中添加树形列表

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,5 +1,7 @@
 using Microsoft.UI.Windowing;
 using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
+using System.Collections.ObjectModel;
 
 namespace SEH
 {
@@ -8,6 +10,9 @@ namespace SEH
     /// </summary>
     public partial class MainWindow : Window
     {
+        public ObservableCollection<ExplorerItem> DataSource { get; set; }
+
+
         public MainWindow()
         {
             InitializeComponent();
@@ -25,6 +30,8 @@ namespace SEH
             AppWindow.SetPresenter(presenter);
 
             this.Activated += MainWindow_Activated;
+
+            DataSource = GetData();
         }
 
         private void MainWindow_Activated(object sender, WindowActivatedEventArgs args)
@@ -33,5 +40,80 @@ namespace SEH
             presenter.Maximize();
         }
 
+        private ObservableCollection<ExplorerItem> GetData()
+        {
+            return new ObservableCollection<ExplorerItem>
+            {
+                new ExplorerItem
+                {
+                    Name = "Documents",
+                    Type = ExplorerItem.ExplorerItemType.Folder,
+                    Children =
+                    {
+                        new ExplorerItem
+                        {
+                            Name = "ProjectProposal",
+                            Type = ExplorerItem.ExplorerItemType.File,
+                        },
+                        new ExplorerItem
+                        {
+                            Name = "BudgetReport",
+                            Type = ExplorerItem.ExplorerItemType.File,
+                        },
+                    },
+                },
+                new ExplorerItem
+                {
+                    Name = "Projects",
+                    Type = ExplorerItem.ExplorerItemType.Folder,
+                    Children =
+                    {
+                        new ExplorerItem
+                        {
+                            Name = "Project Plan",
+                            Type = ExplorerItem.ExplorerItemType.File,
+                        },
+                    },
+                },
+            };
+        }
+
     }
+
+
+    public class ExplorerItem
+    {
+        public enum ExplorerItemType
+        {
+            Folder,
+            File,
+        }
+
+        public string Name { get; set; } = string.Empty;
+        public ExplorerItemType Type { get; set; }
+        public ObservableCollection<ExplorerItem> Children { get; set; } = new ObservableCollection<ExplorerItem>();
+    }
+
+    class ExplorerItemTemplateSelector : DataTemplateSelector
+    {
+        // Template to use for folder items in the TreeView.
+        public DataTemplate? FolderTemplate { get; set; }
+
+        // Template to use for file items in the TreeView.
+        public DataTemplate? FileTemplate { get; set; }
+
+        // Determines which template to use for each item in the TreeView based on its type.
+        protected override DataTemplate? SelectTemplateCore(object item)
+        {
+            // Cast the item to the ExplorerItem type.
+            var explorerItem = (ExplorerItem)item;
+
+            // Return the appropriate template: FolderTemplate for folders, FileTemplate for files.
+            return explorerItem.Type == ExplorerItem.ExplorerItemType.Folder
+                ? FolderTemplate
+                : FileTemplate;
+        }
+    }
+
+
 }
