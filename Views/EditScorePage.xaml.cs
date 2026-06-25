a09f8a7f4da98c5b7a33a5720d23a497a54740c0
zjchenxk <zjchenxk@163.com>
2026-06-25 21:45:09 +0800

    在EditScorePage.xaml中添加简谱绘制区

--- Views/EditScorePage.xaml.cs
+++ Views/EditScorePage.xaml.cs
@@ -2,6 +2,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.UI.Xaml.Controls;
 using Newtonsoft.Json.Linq;
 using SEH.ViewModels;
+using Windows.Graphics.Display;
 
 namespace SEH.Views
 {
@@ -21,7 +22,32 @@ namespace SEH.Views
             //初始化组件（XAML UI 元素）
             InitializeComponent();
 
+            this.Loaded += EditScorePage_Loaded;
+
             this.DataContext = ViewModel;
+        }
+
+        private void EditScorePage_Loaded(object sender, Microsoft.UI.Xaml.RoutedEventArgs e)
+        {
+            // 检查 XamlRoot 是否可用
+            if (this.XamlRoot != null)
+            {
+                // 获取当前系统的缩放比例 (1.0 = 96 DPI, 1.25 = 120 DPI, 1.5 = 144 DPI 等)
+                float scaleFactor = (float)this.XamlRoot.RasterizationScale;
+                float dpi = 96 * scaleFactor;
+
+                // A4 尺寸 (毫米)
+                double widthInMm = 210;
+                double heightInMm = 297;
+
+                // 转换为像素
+                double widthInPixels = (widthInMm / 25.4) * dpi;
+                double heightInPixels = (heightInMm / 25.4) * dpi;
+
+                // 假设你的 Canvas 名字是 ScoreCanvas
+                ScoreCanvas.Width = widthInPixels;
+                ScoreCanvas.Height = heightInPixels;
+            }
         }
 
         protected override void OnNavigatedTo(Microsoft.UI.Xaml.Navigation.NavigationEventArgs e)
@@ -34,7 +60,6 @@ namespace SEH.Views
                 ViewModel.Initialize((JObject)e.Parameter);
             }
         }
-
 
     }
 }
