c3ed8e7f2a24dbda8fc2a837dee3259710cafa50
zjchenxk <zjchenxk@163.com>
2026-07-18 17:35:13 +0800

    修改MultiSelectComboBox为ListView

--- Views/EditNoteDialog.xaml.cs
+++ Views/EditNoteDialog.xaml.cs
@@ -1,4 +1,5 @@
 using Microsoft.UI.Xaml.Controls;
+using SEH.Models;
 using SEH.ViewModels;
 
 namespace SEH.Views
@@ -22,5 +23,46 @@ namespace SEH.Views
             this.DataContext = ViewModel;
         }
 
+        private void StartSlursListView_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            var listView = sender as ListView;
+            if (listView != null)
+            {
+                //获取当前选中的项
+                var selectedItems = listView.SelectedItems;
+                if (selectedItems != null && selectedItems.Count > 0)
+                {
+                    //清空并更新 ViewModel 中的选中集合
+                    ViewModel.SelectedStartSlurs ??= [];
+                    ViewModel.SelectedStartSlurs?.Clear();
+
+                    foreach (var item in selectedItems)
+                    {
+                        ViewModel.SelectedStartSlurs?.Add((Slur)item);
+                    }
+                }
+            }
+        }
+
+        private void EndSlursListView_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            var listView = sender as ListView;
+            if (listView != null)
+            {
+                //获取当前选中的项
+                var selectedItems = listView.SelectedItems;
+                if (selectedItems != null && selectedItems.Count > 0)
+                {
+                    //清空并更新 ViewModel 中的选中集合
+                    ViewModel.SelectedEndSlurs ??= [];
+                    ViewModel.SelectedEndSlurs?.Clear();
+
+                    foreach (var item in selectedItems)
+                    {
+                        ViewModel.SelectedEndSlurs?.Add((Slur)item);
+                    }
+                }
+            }
+        }
     }
 }
